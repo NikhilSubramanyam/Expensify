d73fcab6490a5067682d2eebfa63b73f169e4a09
NikhilSubramanyam <nikhil.subramanyam1@gmail.com>
2020-03-03 21:51:00 -0600

    Lines starting

--- src/app.js
+++ src/app.js
@@ -1,25 +1,7 @@
-//import './util.js';
-// import subtract,{square,add} from './util.js';
+import React from "react";
+import ReactDOM from "react-dom";
+import IndecisionApp from "./components/IndecisionApp";
+import "normalize.css/normalize.css";
+import "./styles/styles.scss";
 
-
-// console.log("app.js is running");
-// console.log(square(4));
-// console.log(add(11,11));
-//console.log(subtract(100,80));
-
-// import "./person.js";
-// import isSenior,{ isAdult, canDrink } from "./person.js";
-// console.log(isAdult(17));
-// console.log(canDrink(17));
-// console.log(isSenior(65));
-
-//install -> import -> use (npm validator)->Google it 
-// import validator from 'validator';
-
-// console.log(validator.isEmail('test@gmail.com'));
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const template = React.createElement('p', {}, 'testing 123');
-ReactDOM.render(template,document.getElementById('app'));
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
--- src/person.js
+++
@@ -1,4 +0,0 @@
-const isAdult = age => (age >= 18 ? true : false);
-const canDrink = age => (age >= 21 ? true : false);
-const isSeniorCitizen = age => (age >= 65 ? true : false);
-export { isAdult, canDrink,isSeniorCitizen as default };
